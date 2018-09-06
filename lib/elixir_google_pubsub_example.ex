07cc1861e800cf1c1c89fda5f9dc9010864ae659
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-06 12:06:15 -0300

    create elixir application

---
+++ lib/elixir_google_pubsub_example.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGooglePubsubExample do
+  @moduledoc """
+  Documentation for ElixirGooglePubsubExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGooglePubsubExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_google_pubsub_example_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGooglePubsubExampleTest do
+  use ExUnit.Case
+  doctest ElixirGooglePubsubExample
+
+  test "greets the world" do
+    assert ElixirGooglePubsubExample.hello() == :world
+  end
+end
