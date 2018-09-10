a3ee0c0ae7f2b660038c2f02021c4a85e239e7a5
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-10 11:04:03 -0300

    test pubsub connection

--- lib/elixir_google_pubsub_example.ex
+++ lib/elixir_google_pubsub_example.ex
@@ -15,4 +15,17 @@ defmodule ElixirGooglePubsubExample do
   def hello do
     :world
   end
+
+  def test do
+    IO.puts "set project id"
+    project_id = "my-awesome-project"
+    IO.puts "configure client"
+    client = GoogleApi.PubSub.V1.Connection.new()
+    IO.puts "get topics"
+    {:ok, topics} =
+      client
+      |> GoogleApi.PubSub.V1.Api.Projects.pubsub_projects_topics_list(project_id)
+    IO.puts "show topics"
+    IO.inspect topics
+  end
 end
