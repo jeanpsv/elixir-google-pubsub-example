3ee41469e1664e63969e9592d3f45ab552dd1193
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-12 18:09:29 -0300

    update application variables

--- lib/elixir_google_pubsub_example/publisher.ex
+++ lib/elixir_google_pubsub_example/publisher.ex
@@ -7,7 +7,7 @@ defmodule ElixirGooglePubsubExample.Publisher do
 
   def run do
     interval =
-      Application.get_env(:elixir_google_pubsub_example, :publisher_interval)
+      Application.get_env(:elixir_google_pubsub_example, :interval)
       |> String.to_integer
     receive do
     after
@@ -20,11 +20,12 @@ defmodule ElixirGooglePubsubExample.Publisher do
   defp publish do
     project_id = Application.get_env(:elixir_google_pubsub_example, :gcp_project_id)
     topic_name = Application.get_env(:elixir_google_pubsub_example, :pubsub_topic)
-    publisher_name = Application.get_env(:elixir_google_pubsub_example, :publisher_name)
+    publisher_name = Application.get_env(:elixir_google_pubsub_example, :name)
     current_millis = :os.system_time(:millisecond)
     message = "#{publisher_name} === #{current_millis}"
 
     {:ok, _topic} = GooglePubsub.get_or_create_topic(project_id, topic_name)
     {:ok, _publish_response} = GooglePubsub.publish_message(project_id, topic_name, message)
+    Logger.info message
   end
 end
