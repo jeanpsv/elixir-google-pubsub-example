6a7c9d420c881f412222ddd4fb7278c8af252851
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-12 17:57:52 -0300

    rename variables

--- config/dev.exs
+++ config/dev.exs
@@ -5,8 +5,8 @@ config :elixir_google_pubsub_example,
   gcp_project_id: System.get_env("GCP_PROJECT_ID"),
   pubsub_topic: System.get_env("PUBSUB_TOPIC"),
   pubsub_role: System.get_env("PUBSUB_ROLE") |> String.to_atom,
-  publisher_name: System.get_env("PUBLISHER_NAME"),
-  publisher_interval: System.get_env("PUBLISHER_INTERVAL")
+  name: System.get_env("NAME"),
+  interval: System.get_env("INTERVAL")
 
 config :logger,
   level: :info
